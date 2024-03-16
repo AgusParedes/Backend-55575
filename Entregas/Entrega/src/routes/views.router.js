5d837cb8c52fa313667ed92277fe2b348245cc96
Agustin Paredes <paredesagustin15@gmail.com>
2024-03-16 13:32:26 -0300

    prueba cambio de ruta login

--- Entregas/Entrega/src/routes/views.router.js
+++ Entregas/Entrega/src/routes/views.router.js
@@ -6,7 +6,7 @@ import { Login, Register, RenderHome } from "../controllers/views.controllers.js
 const router = Router();
 
 router.get ('/register', Register) 
-router.get ('/login', Login) 
-router.get('/', passportCall('jwt'),authorization('user','premium', 'admin'), RenderHome);
+router.get ('/', Login) 
+router.get('/home', passportCall('jwt'),authorization('user','premium', 'admin'), RenderHome);
 
 export default router
